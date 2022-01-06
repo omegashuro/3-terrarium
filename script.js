a803a28129678766973081626cb4de8c0ce10b97
Omega Shuro <omegatshuro@gmail.com>
2022-01-07 01:15:40 +0200

    images and css

---
+++ script.js
@@ -0,0 +1,19 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/draggabilly/3.0.0/draggabilly.pkgd.min.js" integrity="sha512-hbEWVzVt2yK7kQmJ1NZirxxO0NPtE4W09fT4f9Emem/n0rMPFHlLbuLcUcLNrtUGWGoQ8qGvKDVJnRRuCzjeZA==" crossorigin="anonymous" referrerpolicy="no-referrer">
+</script>
+
+let plant1 = dragElement(document.getElementById("plant1"));
+let plant2 = dragElement(document.getElementById("plant2"));
+let plant3 = dragElement(document.getElementById("plant3"));
+let plant4 = dragElement(document.getElementById("plant4"));
+let plant5 = dragElement(document.getElementById("plant5"));
+let plant6 = dragElement(document.getElementById("plant6"));
+let plant7 = dragElement(document.getElementById("plant7"));
+let plant8 = dragElement(document.getElementById("plant8"));
+let plant9 = dragElement(document.getElementById("plant9"));
+let plant10 = dragElement(document.getElementById("plant10"));
+let plant11 = dragElement(document.getElementById("plant11"));
+let plant12 = dragElement(document.getElementById("plant12"));
+let plant13 = dragElement(document.getElementById("plant13"));
+let plant14 = dragElement(document.getElementById("plant14"));
+let draggie = new Draggabilly(plant1);
+
